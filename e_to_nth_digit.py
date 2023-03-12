252b6bc87b0879dbe26bb7a6fc341997409ac704
Kanishka.R <111358462+kanish13@users.noreply.github.com>
2023-03-12 21:09:49 +0530

    Update e_to_nth_digit.py

--- e_to_nth_digit.py
+++ e_to_nth_digit.py
@@ -4,5 +4,6 @@ def cal_e_value(num):
     value=round(math.e,num)
     return value
 
-num=int(input("Enter the number till which you want the e value:"))
-print(cal_e_value(num))
+if __name__=="__main__":
+    num=int(input("Enter the number till which you want the e value:"))
+    print(cal_e_value(num))
